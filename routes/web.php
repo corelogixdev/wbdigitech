47c11c62e5194f30f6fd28aab84700e239b50271
hafeez ullah <ullahhafeez321@gmail.com>
2025-08-30 21:17:21 +0500

    initiliazed the project

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.home');
+});
+
+Route::middleware(['auth','admin'])->group(function () {
+
+    Route::get('/admin', function () {
+        
+        return view('pages.admin.dashboard');
+    })->name('dashboard');
+
+});
+
+Route::middleware(['auth','admin'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
