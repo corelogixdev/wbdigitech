ff068ddfd28e47d861e78965b4512e35d8969fe0
hafeez ullah <ullahhafeez321@gmail.com>
2025-09-01 15:09:10 +0500

    footerlayout links fixed

--- resources/views/layouts/footerLayout.blade.php
+++ resources/views/layouts/footerLayout.blade.php
@@ -9,12 +9,12 @@
                                 </div>
                                 <div class="tp-footer-menu menu-anim">
                                     <ul class="counter-row tp-text-anim">
-                                        <li class="active"><a href="https://www.volgatigris.com">Home</a></li>
-                                        <li class="active"><a href="https://www.volgatigris.com/about-us">Our Story</a></li>
-                                        <li class="active"><a href="https://www.volgatigris.com/blog">Blog</a></li>
-                                        <li class="active"><a href="https://www.volgatigris.com/customer">Our Clients</a></li>
-                                        <li class="active"><a href="https://www.volgatigris.com/contact-us">Contact</a></li>
-                                        <li class="active"><a href="https://www.volgatigris.com/career">Careers</a></li>
+                                        <li class="active"><a href="#">Home</a></li>
+                                        <li class="active"><a href="#">About Us</a></li>
+                                        <li class="active"><a href="#">Blog</a></li>
+                                        <li class="active"><a href="#">Our Clients</a></li>
+                                        <li class="active"><a href="#">Contact</a></li>
+                                        <li class="active"><a href="#">Careers</a></li>
                                     </ul>
                                 </div>
                                 <div class="tp-copyright-text mt-50">
@@ -31,7 +31,7 @@
                                                         <div class="container">
                                                             <div class="row page_speed_1944252306">
                                                                 <div class="col-md-12 text-center">
-                                                                    <div class="page_speed_204344997"><img src=/storage/other/logo-1.webp alt="Volga Tigris Logo" class="img-fluid w-50"></div>
+                                                                    <div class="page_speed_204344997"><img src=/storage/other/logo-1.webp alt="WBDigitech Logo" class="img-fluid w-50"></div>
                                                                     <div class="text-white my-1 page_speed_2070794123">Get a call within 55 seconds </div>
                                                                     <div class="my-1 text-white page_speed_1632279822"><small><b>Leave your number below</b><br> and we will call you right away!</small></div>
                                                                 </div>
@@ -39,8 +39,8 @@
                                                         </div>
                                                     </div>
                                                     <div class="modal-body p-4 bg-white">
-                                                        <form action="/contact/send" method="POST"><input type=hidden name=_token value="267MOCfqPhvdLKKTD6nHXNe13fL4fjCQuNmAEByK" autocomplete="off"><input type=hidden name=page_name value="https://www.volgatigris.com"><input type=hidden name=content
-                                                                value="https://www.volgatigris.com">
+                                                        <form action="/contact/send" method="POST"><input type=hidden name=_token value="#" autocomplete="off"><input type=hidden name=page_name value="#"><input type=hidden name=content
+                                                                value="#">
                                                             <div class="container card page_speed_723228071">
                                                                 <div class="row card-body">
                                                                     <div class="col-lg-12 "><input type=hidden name=page_url value="/"><label>Name*</label><input type=text id="Last_Name" name=name maxlength="80" class="form-control" required="" placeholder="Name"></div>
@@ -65,24 +65,24 @@
                                         <div class="tp-footer-widget">
                                             <h2 class="tp-footer-title tp_fade_bottom">Say hello at:</h2>
                                             <div class="tp-footer-widget-info">
-                                                <div class="tp-footer-widget-info-mail tp_fade_bottom"><a href="mailto:info@volgatigris.com">info@volgatigris.com</a></div>
-                                                <div class="tp-footer-widget-info-location tp_fade_bottom"><a href="https://www.google.com/maps/place/Volga+Tigris/@25.2417773,55.3097268,15z/data=!4m2!3m1!1s0x0:0xd210a6a0c54a790d?sa=X&ved=1t:2428&ictx=111" target="_blank">Business Venue Building, <br>Office 117, <br>Oud Metha, Dubai</a></div>
+                                                <div class="tp-footer-widget-info-mail tp_fade_bottom"><a href="mailto:info@wbdigitech.com">info@wbdigitech.com</a></div>
+                                                <div class="tp-footer-widget-info-location tp_fade_bottom"><a href="https://www.google.com/maps/place/" target="_blank">ABC ABC ABC, <br>Office 000, <br>ABC, Dubai</a></div>
                                             </div>
                                         </div>
                                         <div class="tp-footer-widget">
                                             <h2 class="tp-footer-title tp_fade_bottom">Follow us : </h2>
                                             <!--<ul class="tp-footer-widget-social">-->
                                             <!--<div class="col-md-4 text-center text-lg-end text-md-end">-->
-                                            <div class="header-social"><a class="icon-socials" href="https://www.facebook.com/profile.php?id=61573038426926" target="_blank" title="Facebook"><img src=https://www.volgatigris.com/storage/social-icons/facebook.png alt="facebook logo" class="page_speed_405760890"></a>
+                                            <div class="header-social"><a class="icon-socials" href="https://www.facebook.com/" target="_blank" title="Facebook"><img src="{{ asset('storage/social-icons/facebook.png')}}" alt="facebook logo" class="page_speed_405760890"></a>
                                                 <a
-                                                    class="icon-socials" href="https://www.instagram.com/volgatigris/" target="_blank" title="Instagram"><img src=https://www.volgatigris.com/storage/social-icons/social.png alt="social logo" class="page_speed_405760890"></a><a class="icon-socials" href="https://www.linkedin.com/company/volga-tigris-digital-marketing-agency-dubai"
-                                                        target="_blank" title="LinkedIn"><img src=https://www.volgatigris.com/storage/social-icons/linkedin.png alt="linkedin logo" class="page_speed_405760890"></a><a class="icon-socials" href="https://www.youtube.com/@volgatigrisofficial550"
-                                                        target="_blank" title="Youtube"><img src=https://www.volgatigris.com/storage/social-icons/youtube.png alt="youtube logo" class="page_speed_405760890"></a></div>
+                                                    class="icon-socials" href="https://www.instagram.com/" target="_blank" title="Instagram"><img src="{{ asset('storage/social-icons/social.png')}}" alt="social logo" class="page_speed_405760890"></a><a class="icon-socials" href="https://www.linkedin.com/"
+                                                        target="_blank" title="LinkedIn"><img src="{{ asset('storage/social-icons/linkedin.png')}}" alt="linkedin logo" class="page_speed_405760890"></a><a class="icon-socials" href="https://www.youtube.com/"
+                                                        target="_blank" title="Youtube"><img src="{{asset('storage/social-icons/youtube.png')}}" alt="youtube logo" class="page_speed_405760890"></a></div>
                                             <!--</div>-->
                                             <!--</ul>-->
                                         </div>
                                         <div class="tp-footer-widget">
-                                            <div class="embed-responsive embed-responsive-16by9"><iframe title="my business location " src=https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.855922418048!2d55.30715187538327!3d25.24177727768231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5ded50e9cac9%3A0xd210a6a0c54a790d!2sVolga%20Tigris!5e0!3m2!1sen!2sae!4v1732171704762!5m2!1sen!2sae
+                                            <div class="embed-responsive embed-responsive-16by9"><iframe title="my business location " src=https://www.google.com/maps/
                                                     allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="embed-responsive-item page_speed_1429476990"></iframe></div>
                                         </div>
                                     </div>
@@ -96,11 +96,11 @@
                 <div class="tp-copyright-wrap">
                     <div class="row align-items-center">
                         <div class="col-md-6">
-                            <div class="tp-copyright-logo text-center text-md-start"><a href="#"><img src=https://www.volgatigris.com/public/new-assets/img/logo/logo-white.png alt="Volga Tigris Logo White"></a></div>
+                            <div class="tp-copyright-logo text-center text-md-start"><a href="#"><img src="{{ asset('new-assets/img/logo/logo-white.png')}}" alt="WBdigitech Logo White"></a></div>
                         </div>
                         <div class="col-md-6">
                             <div class="tp-copyright-text text-center text-md-end">
-                                <p>Copyright © Volga Tigris. All Rights Reserved</p>
+                                <p>Copyright © Wbdigi Tech. All Rights Reserved</p>
                             </div>
                         </div>
                         <!--<div class="col-md-4">-->
@@ -112,7 +112,7 @@
     </footer>
     </div>
     </div>
-    <div class="modal-wrapper p-relative" data-background="https://www.volgatigris.com/public/new-assets/img/home-01/team/team-details-bg.png">
+    <div class="modal-wrapper p-relative" data-background="{{ asset('new-assets/img/home-01/team/team-details-bg.png')}}">
         <div class="modal fade" id="myModal" tabindex="-1" aria-hidden="true">
             <div class="modal-dialog modal-width pt-200">
                 <div class="modal-content">
@@ -120,13 +120,13 @@
                         <div class="container">
                             <div class="row page_speed_1944252306">
                                 <div class="col-md-12 text-center ">
-                                    <div class="page_speed_204344997"><img src=https://www.volgatigris.com/public/new-assets/img/logo/logo-white.png alt="Volga Tigris Logo" class="img-fluid w-50"></div>
+                                    <div class="page_speed_204344997"><img src="{{ asset('new-assets/img/logo/logo-white.png')}}" alt="Wbdigitech Logo" class="img-fluid w-50"></div>
                                     <div class="d-flex justify-content-center align-items-center">
                                         <!--<div>-->
                                         <!--</div>-->
-                                        <a href="tel:+971506252050" class="d-flex flex-column align-items-center pb-10">
+                                        <a href="tel:+971000000000" class="d-flex flex-column align-items-center pb-10">
                                             <div class="d-flex align-items-center"><i class="fa-solid fa-phone page_speed_946076538"></i><span class="page_speed_1144108505 text-white ml-10">Contact Us Directly</span></div>
-                                            <h3 class="text-white page_speed_1210848034">050 625 2050</h3>
+                                            <h3 class="text-white page_speed_1210848034">000 000 0000</h3>
                                         </a>
                                     </div>
                                 </div>
@@ -134,7 +134,7 @@
                         </div>
                     </div>
                     <div class="modal-body">
-                        <form action="/contact/send" method="POST"><input type=hidden name=_token value="267MOCfqPhvdLKKTD6nHXNe13fL4fjCQuNmAEByK" autocomplete="off"><input type=hidden name=page_name value="https://www.volgatigris.com"><input type=hidden name=content value="https://www.volgatigris.com">
+                        <form action="/contact/send" method="POST"><input type=hidden name=_token value="#" autocomplete="off"><input type=hidden name=page_name value="#"><input type=hidden name=content value="#">
                             <div
                                 class="container card page_speed_723228071">
                                 <div class="row card-body">
@@ -158,11 +158,11 @@
     </div>
     </div>
     </div>
-     <div class="fixed-contact"><a href="tel:+971506252050"><i class="fas fa-phone"></i></a><a href="#" class="open-popup"><i class="fas fa-envelope-open-text"></i></a></div>
+     <div class="fixed-contact"><a href="tel:+971000000000"><i class="fas fa-phone"></i></a><a href="#" class="open-popup"><i class="fas fa-envelope-open-text"></i></a></div>
     <div class="ads-popup">
         <div class="popup-wrapper">
             <div class="popup-head"><button class="popup-close" aria-label="Close popup">X</button></div>
-            <div class="popup-img"><img src=https://www.volgatigris.com/public/new-assets/img/logo/logo.png alt="Volga Tigris Logo"></div>
+            <div class="popup-img"><img src="{{ asset('new-assets/img/logo/logo.png')}}" alt="Wbdigitech Logo"></div>
             <div class="popup-body">
                 <p class="popup-headline">Call Now For The <br> Best Digital Marketing Services</p>
                 <ul class="popup-features">
@@ -170,19 +170,19 @@
                     <li><span>✔</span>24/7 Customer Support</li>
                     <li><span>✔</span>Hassle-Free Services</li>
                 </ul>
-                <div class="popup-cta-grid"><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Digital<br>Marketing</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Google Ranking<br>SEO</a><a href="tel:+971506252050"
-                        class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Web<br>Development</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Google <br>Ads</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Graphic<br>Design</a>
+                <div class="popup-cta-grid"><a href="tel:+971000000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Digital<br>Marketing</a><a href="tel:+971000000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Google Ranking<br>SEO</a><a href="tel:+97100000000"
+                        class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Web<br>Development</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Google <br>Ads</a><a href="tel:+9710000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Graphic<br>Design</a>
                     <a
-                        href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Content<br>Creation</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Data<br>Services</a><a href="tel:+971506252050" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Social Media<br> Marketing</a></div>
+                        href="tel:+971000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Content<br>Creation</a><a href="tel:+971000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Data<br>Services</a><a href="tel:+9710000000" class="popup-cta-btn"><i class="fa-solid fa-phone"></i> Social Media<br> Marketing</a></div>
                 <a
-                    href="tel:+971506252050" class="popup-main-call-btn"><i class="fa-solid fa-phone"></i>+971 50 625 2050</a>
+                    href="tel:+971000000" class="popup-main-call-btn"><i class="fa-solid fa-phone"></i>+971 00 000 0000</a>
             </div>
             <div class="popup-footer"> 24/7 Support </div>
         </div>
     </div>
     <div class="tp-footer-shape-wrap z-index-5 smooth d-none">
         <a href="#">
-            <div class="tp-footer-shape p-relative"><img class="img-1" src=https://www.volgatigris.com/public/new-assets/img/home-01/footer/footer-circle-shape-1.png alt="Volga Tigris "><img class="img-2" src=https://www.volgatigris.com/public/new-assets/img/home-01/footer/footer-circle-shape-2.png
-                    alt="Volga Tigris"><span></span></div>
+            <div class="tp-footer-shape p-relative"><img class="img-1" src="{{ asset('new-assets/img/home-01/footer/footer-circle-shape-1.png') }}" alt="Wbdigitech"><img class="img-2" src="{{ asset('new-assets/img/home-01/footer/footer-circle-shape-2.png')}}"
+                    alt="Wbdigitech"><span></span></div>
         </a>
     </div>
