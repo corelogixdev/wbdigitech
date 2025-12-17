c5dbc04a3a5f9cc2ea4fa8a358a74ee6ccae39f7
Hafeez Ullah <ullahhafeez321@gmail.com>
2025-12-18 00:08:05 +0500

    changed the homeLayout commented some unused things for load optimization

--- resources/views/layouts/homelayout.blade.php
+++ resources/views/layouts/homelayout.blade.php
@@ -10,15 +10,15 @@
 
 
     <!-- External Libraries -->
-        <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        {{-- <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
         crossorigin=anonymous referrerpolicy="no-referrer">
         <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
-        crossorigin=anonymous referrerpolicy="no-referrer">
+        crossorigin=anonymous referrerpolicy="no-referrer"> --}}
         <link rel="stylesheet" href="{{ asset('vendor/core/plugins/language/css/language-public.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/core/core/base/libraries/ckeditor/content-styles.css') }}">
+        {{-- <link rel="stylesheet" href="{{ asset('vendor/core/core/base/libraries/ckeditor/content-styles.css') }}"> --}}
         <link rel="stylesheet" href="{{ asset('themes/agon/css/style.css') }}">
-        <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Big+Shoulders+Display:wght@400;700&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Big+Shoulders+Display:wght@400;700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet"> 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"  referrerpolicy="no-referrer" />
    
         <!-- Favicon -->
@@ -60,30 +60,30 @@
         {{-- External JS --}}
         {{-- Vendor / Core --}}
     <script src="{{ asset('themes/agon/new-assets/js/vendor/jquery.js') }}"></script>
-    <script src="{{ asset('themes/agon/new-assets/js/bootstrap-bundle.js') }}" defer></script>
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/bootstrap-bundle.js') }}" defer></script> --}}
     <script src="{{ asset('themes/agon/new-assets/js/gsap.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-to-plugin.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-smoother.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/tween-max.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/gsap-split-text.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/chroma.min.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-trigger.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/scroll-magic.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/range-slider.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/swiper-bundle.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/slick.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/three.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/magnific-popup.js') }}" defer></script>
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-to-plugin.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-smoother.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/tween-max.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/gsap-split-text.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/chroma.min.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/gsap-scroll-trigger.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/scroll-magic.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/range-slider.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/swiper-bundle.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/slick.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/three.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/magnific-popup.js') }}" defer></script> --}}
     <script src="{{ asset('themes/agon/new-assets/js/nice-select.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/purecounter.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/beforeafter.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/isotope-pkgd.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/imagesloaded-pkgd.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/ajax-form.js') }}" defer></script>
-    <script src="{{ asset('themes/agon/new-assets/js/webgl.js') }}" defer></script>
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/purecounter.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/beforeafter.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/isotope-pkgd.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/imagesloaded-pkgd.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/ajax-form.js') }}" defer></script> --}}
+    {{-- <script src="{{ asset('themes/agon/new-assets/js/webgl.js') }}" defer></script> --}}
     <script src="{{ asset('themes/agon/new-assets/js/tp-cursor.js') }}" defer></script>
     <script src="{{ asset('themes/agon/new-assets/js/main.js') }}" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> --}}
   
     <!-- Custom JS -->
     <script src="{{ asset('js/custom.js') }}"></script>
@@ -93,8 +93,8 @@
 
 
     <noscript><iframe src=https://www.googletagmanager.com/ns.html?id=GTM-KB33VJ6height="0" width=0 class="page_speed_1689186421"></iframe></noscript>
-    <script src=https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
-        crossorigin=anonymous referrerpolicy="no-referrer" defer></script>
+    {{-- <script src=https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin=anonymous referrerpolicy="no-referrer" defer></script> --}}
 
 
 <!-- Hidden container for Google Translate -->
